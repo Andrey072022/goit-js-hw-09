8d17f83092c025fbc8698caa9dd507f762309a12
Andrey072022 <109408740+Andrey072022@users.noreply.github.com>
2023-04-06 15:31:15 +0000

    Deploying to gh-pages from @ Andrey072022/goit-js-hw-09@d9f243a59a75ccec307141ebb7038a90ee30342c 🚀

---
+++ 01-color-switcher.e155fc3d.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]");let d=null;t.disabled=!0;const a=()=>{document.body.style.background=`#${Math.floor(16777215*Math.random()).toString(16)}`};e.addEventListener("click",(e=>{e.target.disabled=!0,t.disabled=!1,d=setInterval(a,1e3)})),t.addEventListener("click",(()=>{e.disabled=!1,t.disabled=!0,clearInterval(d)}));
+//# sourceMappingURL=01-color-switcher.e155fc3d.js.map
