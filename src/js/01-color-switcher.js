d9f243a59a75ccec307141ebb7038a90ee30342c
Svyntsov Andriy <svincov_a@ukr.net>
2023-04-06 18:30:49 +0300

    06/04/2023

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,30 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+// const bodyEl = document.querySelector('body');
+let idInterval = null;
+stopBtn.disabled = true;
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+
+const changeColor = () => {
+  document.body.style.background = getRandomHexColor();
+};
+
+const handleClickStart = e => {
+  e.target.disabled = true;
+  stopBtn.disabled = false;
+
+  idInterval = setInterval(changeColor, 1000);
+};
+
+const handleClickStop = () => {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+
+  clearInterval(idInterval);
+};
+
+startBtn.addEventListener('click', handleClickStart);
+stopBtn.addEventListener('click', handleClickStop);
